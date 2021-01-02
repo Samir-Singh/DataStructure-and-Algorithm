10e23592937de916b5edaac3b030669808bac4c4
Samir Singh <41338872+Samir-Singh@users.noreply.github.com>
2021-01-02 22:49:15 +0530

    Add files via upload

---
+++ String/Reverse a String-2.c
@@ -0,0 +1,45 @@
+/*
+we have to reverse string but not the special character
+ex : str = a,bc$d!        o/p = d,cb$a!
+     str = a!b*cdf$s      o/p = s!f*dcb$a
+ */
+#include<stdio.h>
+#include<string.h>
+void swap(char *a, char *b)
+{
+	char temp;
+	temp=*a;
+	*a=*b;
+	*b=temp;
+}
+int isAlphabet(char str)
+{
+    if((str >= 'A' && str <= 'Z') || (str >= 'a' && str <= 'z'))
+    	return 1;
+    return 0;
+}
+void reverseString(char *str)
+{
+	int l,r;
+	l=0,r=strlen(str);
+	while(l < r)
+	{
+		if(!isAlphabet(str[l]))
+			l++;
+		if(!isAlphabet(str[r]))
+			r--;
+		else
+		{
+			swap(&str[l],&str[r]);
+			l++;
+			r--;
+		}
+	}
+}
+int main()
+{
+	char str[]="a!b*cdf$s";
+	reverseString(str);
+	printf("%s",str);
+	return 0;
+}
